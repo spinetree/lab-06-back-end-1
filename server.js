5a21b16df3a11c43c56cf675cef1d73c0334d436
spinetree <steve@spinetree.org>
2019-09-18 15:52:21 -0700

    branch for notes and refactor do not merge without checkint

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+
+// this allows us to call express functions via app.get etc.
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT || 3000;
+
+// making this 3001 means you can see if it's getting env variables OR just defaulting
+const PORT = process.env.PORT || 3001;
 
 
 
@@ -24,20 +28,29 @@ app.get('/weather', getWeather);
 function getLocation(request, response) {
   let searchQuery = request.query.data;
   console.log(searchQuery)
+
+  // the api request that we make
   let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${process.env.GEOCODE_API_KEY}`;
 
   // console.log(superagentResults);
 
   superagent.get(url).then(superagentResults => {
+  // superagent makes an api call for us and using .get
+
     let results = superagentResults.body.results[0];
     // console.log('results: ', results)
+
     const formatted_address = results.formatted_address;
     const lat = results.geometry.location.lat;
     const long = results.geometry.location.lng;
+    // get that data back in the form of results and turn these parts of that big object into variables
+    
     const newLocation = new Location(searchQuery, formatted_address, lat, long);
-
+    // make a new location object using the data from the api's response and feed it into the constructor function
 
     response.send(newLocation);
+    // send that data to the frontend
+
   }).catch(error => {
     console.log('=====================halp');
     console.error(error);
@@ -46,23 +59,19 @@ function getLocation(request, response) {
 
 }
 
+
+
 function getWeather(request, response) {
-
-  console.log('This is the response derp :');
-
-  // let requestQueryData = request.query.data;
 
   let lat = request.query.data.latitude;
   let long = request.query.data.latitude;
+  // read in the args
 
   const weatherUrl = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${lat},${long}`;
-
-  // const darkSkyResults = require('./data/darksky.json');
+  // make the URL for hitting the API
 
   superagent.get(weatherUrl).then(superagentResults => {
-
-    // console.log(superagentResults.body.daily.data[0]);
-
+  
     let darkskyDataArray = superagentResults.body.daily.data;
 
     const dailyArray = darkskyDataArray.map(day => {
@@ -72,25 +81,30 @@ function getWeather(request, response) {
   }) .catch(error => {
 
     response.status(500).send(error.message);
-    console.error(error);
+    // console.error(error);
 
   });
-
 }
 
 
+// ------ get Eventbrite events
 
+functiongetEvents(request, response) {
 
-//callback for /weather route
+  let searchQuery = request.query.data;
+  console.log(locationObj);
+  const url = `https://www.eventbriteapi.com/v3/events/search?token=${EVENTS_API_KEY}&location.address=${locationObj.formatted_address}`;
 
-// function getWeather(request, response){
-//   const darkSkyResults = require('./data/darksky.json');
-//   let darkskyDataArray = darkSkyResults.daily.data;
-//   const dailyArray = darkskyDataArray.map(day => {
-//     return new Weather(day)
-//   })
-//   response.send(dailyArray);
-// }
+superagent.get(url
+  .then(eventBriteData => { 
+    const eventBriteInfo = eventBriteData.body.events.map(eventData => {
+      return event;
+    });
+  })
+)
+
+};
+
 
 // ------ END -----
 
@@ -107,6 +121,13 @@ function Weather(darkskyData) {
   this.time = new Date(darkskyData.time * 1000).toDateString();
   // this.time = new Date(darkskyData.time).toDateString();
   this.forecast = darkskyData.summary;
+}
+
+functionEvent(eventBriteStuf) {
+  this.link = eventBriteStuff.url;
+  this.name = eventBriteStuff.name.text;
+  this.event_date = new Date(eventBriteStuff.start.local).toDateString();
+  this.summary = eventBriteStuff.summary;
 }
 
 // --------- END --------
